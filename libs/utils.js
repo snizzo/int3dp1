63fcc7d3e7a754d0321f41dd427c36bdf7eb7968
Claudio Desideri <happy.snizzo@gmail.com>
2014-04-02 18:45:20 +0200

    opacity material fixes, pie chart finalized. Feature freeze on everything. Next goal interface.

--- libs/utils.js
+++ libs/utils.js
@@ -1,34 +1,34 @@
 
 function getRandomColor(i) {
-    var colors = [];
-    var lcolors = [];
-    
-    //default good colors
-    colors.push("#DBDB00");
-    colors.push("#54DB00");
-    colors.push("#00DBD8");
-    colors.push("#0045DB");
-    colors.push("#CF007F");
-    colors.push("#DB7700");
-    
-    
-    //default +30% brighter good colors
-    lcolors.push("#ffff00"); 
+	var colors = [];
+	var lcolors = [];
+
+	//default good colors
+	colors.push("#DBDB00");
+	colors.push("#54DB00");
+	colors.push("#00DBD8");
+	colors.push("#0045DB");
+	colors.push("#CF007F");
+	colors.push("#DB7700");
+
+
+	//default +30% brighter good colors
+	lcolors.push("#ffff00"); 
 	lcolors.push("#6dff00"); 
 	lcolors.push("#00ffff"); 
 	lcolors.push("#005aff"); 
 	lcolors.push("#ff00a5");
 	lcolors.push("#ff9b00"); 
 
-    
-    //choose one based on a seed
-    var r = i % colors.length;
-    
-    var results = [];
-    results.push(colors[r]);
-    results.push(lcolors[r]);
-    
-    return results;
+
+	//choose one based on a seed
+	var r = i % colors.length;
+
+	var results = [];
+	results.push(colors[r]);
+	results.push(lcolors[r]);
+
+	return results;
 }
 
 function onWindowResize() 
